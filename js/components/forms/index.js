486227c7aedc5c9669d2811c0bdd50c09e08d6d6
Alberto Contreras <a.contreras@catchdigital.com>
2019-12-13 10:09:25 +0000

    Add forms javascript to dist folder.

---
+++ js/components/forms/index.js
@@ -0,0 +1,3 @@
+import Forms from 'materialize-css/js/forms.js';
+
+const instance = M.Tabs.init(el, options);
--- js/components/tabs/index.js
+++ js/components/tabs/index.js
@@ -1,1 +1,3 @@
-var instance = M.Tabs.init(el, options);
+import Tabs from 'materialize-css/js/tabs.js';
+
+const instance = M.Tabs.init(el, options);
