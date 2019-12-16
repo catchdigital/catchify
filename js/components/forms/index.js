5ad0e19af92f49d3201b6f573b8db9827870e90b
Alberto Contreras <a.contreras@catchdigital.com>
2019-12-16 16:00:50 +0000

    Add support for date and time pickers.

--- js/components/forms/index.js
+++ js/components/forms/index.js
@@ -1,10 +1,26 @@
 import {
     Forms,
     Select,
-    Dropdown
+    Dropdown,
+    Modal,
+    Datepicker,
+    Timepicker
 } from 'materialize-css';
 
 document.addEventListener('DOMContentLoaded', function() {
-  const elems = document.querySelectorAll('select');
-  const instances = M.FormSelect.init(elems, {});
+  // Init select box
+  const selects = document.querySelectorAll('select');
+  const selectInstances = M.FormSelect.init(selects, {});
+
+  // Init datepicker
+  // The format has been set up following Drupal time field standards.
+  // TODO: Create a fallback which takes the value from the field data attrs.
+  const datepickers = document.querySelectorAll('.datepicker');
+  const datepikerInstances = M.Datepicker.init(datepickers, {'format': 'yyyy-m-d'});
+
+  // Init timepicker
+  // Twelve hours type isn't supported by Drupal..
+  // TODO: Create a fallback function that convert the value to requirements.
+  const timepickers = document.querySelectorAll('.timepicker');
+  const timepickerInstances = M.Timepicker.init(timepickers, {'twelveHour': false});
 });
