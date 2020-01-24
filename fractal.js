2a82648ce2027c1dec2baa5a2f6d7c1ee7fc6908
Alberto Contreras <a.contreras@catchdigital.com>
2020-01-24 10:54:04 +0000

    Add fractal to the project.

---
+++ fractal.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/*
+* Require the path module
+*/
+const path = require('path');
+const root = __dirname + '/framework'
+
+/*
+ * Require the Fractal module
+ */
+const fractal = module.exports = require('@frctl/fractal').create();
+
+/*
+ * Give your project a title.
+ */
+fractal.set('project.title', 'Catchify');
+
+/*
+ * Tell Fractal where to look for components.
+ */
+fractal.components.set('path', path.join(root, 'components'));
+
+/*
+ * Tell Fractal where to look for documentation pages.
+ */
+fractal.docs.set('path', path.join(root, 'docs'));
+
+/*
+ * Tell the Fractal web preview plugin where to look for static assets.
+ */
+fractal.web.set('static.path', path.join(root, 'public'));
