4aea75d70279a8b645b58551fa979e3b76d45b82
Alberto Contreras <abecontreras@Albertos-MacBook-Pro.local>
2020-01-26 16:57:53 +0000

    Update path for local and add docroot listing on build.

--- fractal.js
+++ fractal.js
@@ -30,7 +30,7 @@ fractal.docs.set('path', path.join(root, 'docs'));
 /*
  * Tell the Fractal web preview plugin where to look for static assets.
  */
-fractal.web.set('static.path', path.join(root, 'dist'));
+fractal.web.set('static.path', path.join(__dirname, 'dist'));
 
 // Builder config
 fractal.web.set('builder.dest', path.join(__dirname, '/docroot'));
