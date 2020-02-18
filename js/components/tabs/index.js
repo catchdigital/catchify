73c6d4b9bc6413d9fe8d1101d6a21710650e4c1f
Alberto Contreras <a.contreras@catchdigital.com>
2020-02-18 16:43:18 +0000

    Fix tabs js call and update version.

--- js/components/tabs/index.js
+++ js/components/tabs/index.js
@@ -1,6 +1,6 @@
 import { Tabs } from 'materialize-css';
 
 document.addEventListener('DOMContentLoaded', function() {
-  const el = document.querySelectorAll('tabs');
+  const el = document.querySelectorAll('.tabs');
   const instance = M.Tabs.init(el, {});
 });
