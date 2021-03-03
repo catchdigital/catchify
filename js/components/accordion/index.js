62da23c0bcbd4e05122c5e6169a7db84fdd46919
George Phillips <g.phillips@catchdigital.com>
2021-03-03 16:53:53 +0000

    CATCH-698: Updated accordion to blur trigger element not e.target.

--- js/components/accordion/index.js
+++ js/components/accordion/index.js
@@ -70,7 +70,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
   const handleButtonClick = (e) => {
     const $element = e.target.closest(SELECTOR.ACCORDION);
-    e.target.blur();
+    $element.querySelector(SELECTOR.TRIGGER).blur();
 
     if ($element.classList.contains(CLASS.MOBILE_ONLY)) {
       if (isMobile()) {
