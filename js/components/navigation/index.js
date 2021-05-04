1fc2a59fbe5e27804f03630f44794a0c24613299
Dom Gaulton <domgaulton@Doms-MacBook-Pro.local>
2021-05-04 16:27:24 +0100

    navigation bare bones

--- js/components/navigation/index.js
+++ js/components/navigation/index.js
@@ -1,6 +1,4 @@
-import a11yClick from '../helpers/a11yClick.js';
-
-export default () => {
+document.addEventListener('DOMContentLoaded', function() {
   const ATTR = {
     MAIN_MENU: 'data-main-menu',
     MAIN_MENU_DEVICE_TOGGLE: 'data-main-menu--device-toggle',
@@ -50,96 +48,106 @@ export default () => {
     })
   }
 
-  $(document).ready(() => {
-    
-    const mainMenu = $(SELECTOR.MAIN_MENU);
-    const mainMenuDeviceToggle = $(SELECTOR.MAIN_MENU_DEVICE_TOGGLE);
-    const mainMenuLinks = $(SELECTOR.MAIN_MENU_LINK);
-    const subMenus = $(SELECTOR.MAIN_MENU_SUBMENU);
-    const backButton = $(SELECTOR.MAIN_MENU_BACK_BUTTON);
-
-    // AKA Burger Menu
-    mainMenuDeviceToggle.click(function() {
-      resetLinksActiveState(mainMenuLinks)
-      resetSubMenusActiveState(subMenus)
-
-      if ( mainMenu.hasClass(CLASSES.IS_ACTIVE) ) {
-        console.log('has class')
-        $(this).removeClass(CLASSES.IS_ACTIVE)
-        mainMenu.removeClass(CLASSES.IS_ACTIVE);
-        $(this).attr('aria-expanded', 'false');
-      } else {
-        mainMenu.addClass(CLASSES.IS_ACTIVE);
-        $(this).addClass(CLASSES.IS_ACTIVE)
-        $(this).attr('aria-expanded', 'true');
+  const a11yClick = event => {
+    if (event.type === 'click') {
+      return true;
+    } else if (event.type === 'keypress') {
+      let code = event.charCode || event.keyCode;
+      if (code === 32 || code === 13) {
+        return true;
       }
-    })
+    } else {
+      return false;
+    }
+  };
+  
+  const mainMenu = $(SELECTOR.MAIN_MENU);
+  const mainMenuDeviceToggle = $(SELECTOR.MAIN_MENU_DEVICE_TOGGLE);
+  const mainMenuLinks = $(SELECTOR.MAIN_MENU_LINK);
+  const subMenus = $(SELECTOR.MAIN_MENU_SUBMENU);
+  const backButton = $(SELECTOR.MAIN_MENU_BACK_BUTTON);
 
-    // Inside each submenu there is a back button for mobile
-    backButton.click(function(e) {
+  // AKA Burger Menu
+  mainMenuDeviceToggle.click(function() {
+    resetLinksActiveState(mainMenuLinks)
+    resetSubMenusActiveState(subMenus)
+
+    if ( mainMenu.hasClass(CLASSES.IS_ACTIVE) ) {
+      console.log('has class')
+      $(this).removeClass(CLASSES.IS_ACTIVE)
+      mainMenu.removeClass(CLASSES.IS_ACTIVE);
+      $(this).attr('aria-expanded', 'false');
+    } else {
+      mainMenu.addClass(CLASSES.IS_ACTIVE);
+      $(this).addClass(CLASSES.IS_ACTIVE)
+      $(this).attr('aria-expanded', 'true');
+    }
+  })
+
+  // Inside each submenu there is a back button for mobile
+  backButton.click(function(e) {
+    e.stopPropagation();
+    const key = $(this).attr(ATTR.MAIN_MENU_BACK_BUTTON);
+
+    const otherLinksAtThisLevel = $(`[${ATTR.MAIN_MENU_LINK_LEVEL}='${key}']`);
+    const otherSubMenusAtThisLevel = $(`[${ATTR.MAIN_MENU_SUBMENU_LEVEL}='${key}']`);
+
+    resetLinksActiveState(otherLinksAtThisLevel)
+    resetSubMenusActiveState(otherSubMenusAtThisLevel)
+  })
+
+  mainMenuLinks.each(function() {
+    
+    $(this).on('click keypress', function(e) {
+      if (!a11yClick(e)) return;
+
       e.stopPropagation();
-      const key = $(this).attr(ATTR.MAIN_MENU_BACK_BUTTON);
 
-      const otherLinksAtThisLevel = $(`[${ATTR.MAIN_MENU_LINK_LEVEL}='${key}']`);
-      const otherSubMenusAtThisLevel = $(`[${ATTR.MAIN_MENU_SUBMENU_LEVEL}='${key}']`);
+      // Are we re clicking an active link?
+      const currentlyActiveLink = $(this).hasClass(CLASSES.IS_ACTIVE);
+
+      // Get data attributes
+      const key = $(this).attr(ATTR.MAIN_MENU_LINK);
+      const thisLevel = $(this).attr(ATTR.MAIN_MENU_LINK_LEVEL);
+
+      // Get elements at this level and down
+      const parentMenuOfLink = $(this).closest(SELECTOR.MAIN_MENU_SUBMENU)
+      const otherLinksAtThisLevel = $(`[${ATTR.MAIN_MENU_LINK_LEVEL}='${thisLevel}']`);
+      const otherSubMenusAtThisLevel = $(`[${ATTR.MAIN_MENU_SUBMENU_LEVEL}='${thisLevel}']`);
+      const directSubMenu = $(`[${ATTR.MAIN_MENU_SUBMENU}='${key}']`);
+      const allChildLinks = $(`[${ATTR.MAIN_MENU_LINK_LEVEL}='1'], [${ATTR.MAIN_MENU_LINK_LEVEL}='2']`);
+      const allChildSubMenus = $(`[${ATTR.MAIN_MENU_SUBMENU_LEVEL}='1'], [${ATTR.MAIN_MENU_SUBMENU_LEVEL}='2']`);
 
       resetLinksActiveState(otherLinksAtThisLevel)
       resetSubMenusActiveState(otherSubMenusAtThisLevel)
-    })
 
-    mainMenuLinks.each(function() {
-      
-      $(this).on('click keypress', function(e) {
-        if (!a11yClick(e)) return;
+      // reset all links;
+      mainMenuLinks.each(function() {
+        $(this).attr('aria-expanded', 'false');
+      })
 
-        e.stopPropagation();
+      // if currently active add / remove classes
 
-        // Are we re clicking an active link?
-        const currentlyActiveLink = $(this).hasClass(CLASSES.IS_ACTIVE);
+      if ( !currentlyActiveLink ) {
+        $(this).attr('aria-expanded', 'true');
+        $(this).addClass(CLASSES.IS_ACTIVE);
+        $(directSubMenu).addClass(CLASSES.IS_ACTIVE)
+      } else {
+        $(this).removeClass(CLASSES.IS_ACTIVE);
+        $(directSubMenu).removeClass(CLASSES.IS_ACTIVE);
+      }
 
-        // Get data attributes
-        const key = $(this).attr(ATTR.MAIN_MENU_LINK);
-        const thisLevel = $(this).attr(ATTR.MAIN_MENU_LINK_LEVEL);
+      // Clicking on top level main nav items
+      if ( thisLevel === '0' ) {
+        resetLinksActiveState(allChildLinks)
+        resetSubMenusActiveState(allChildSubMenus)
+      }
 
-        // Get elements at this level and down
-        const parentMenuOfLink = $(this).closest(SELECTOR.MAIN_MENU_SUBMENU)
-        const otherLinksAtThisLevel = $(`[${ATTR.MAIN_MENU_LINK_LEVEL}='${thisLevel}']`);
-        const otherSubMenusAtThisLevel = $(`[${ATTR.MAIN_MENU_SUBMENU_LEVEL}='${thisLevel}']`);
-        const directSubMenu = $(`[${ATTR.MAIN_MENU_SUBMENU}='${key}']`);
-        const allChildLinks = $(`[${ATTR.MAIN_MENU_LINK_LEVEL}='1'], [${ATTR.MAIN_MENU_LINK_LEVEL}='2']`);
-        const allChildSubMenus = $(`[${ATTR.MAIN_MENU_SUBMENU_LEVEL}='1'], [${ATTR.MAIN_MENU_SUBMENU_LEVEL}='2']`);
-
-        resetLinksActiveState(otherLinksAtThisLevel)
-        resetSubMenusActiveState(otherSubMenusAtThisLevel)
-
-        // reset all links;
-        mainMenuLinks.each(function() {
-          $(this).attr('aria-expanded', 'false');
-        })
-
-        // if currently active add / remove classes
-
-        if ( !currentlyActiveLink ) {
-          $(this).attr('aria-expanded', 'true');
-          $(this).addClass(CLASSES.IS_ACTIVE);
-          $(directSubMenu).addClass(CLASSES.IS_ACTIVE)
-        } else {
-          $(this).removeClass(CLASSES.IS_ACTIVE);
-          $(directSubMenu).removeClass(CLASSES.IS_ACTIVE);
-        }
-
-        // Clicking on top level main nav items
-        if ( thisLevel === '0' ) {
-          resetLinksActiveState(allChildLinks)
-          resetSubMenusActiveState(allChildSubMenus)
-        }
-
-        // Clicking on second level items 
-        if ( thisLevel === '1' ) {
-          checkIfItemsHaveClass(otherLinksAtThisLevel, CLASSES.IS_ACTIVE) ? parentMenuOfLink.addClass(CLASSES.HAS_SUBMENU_ACTIVE) : parentMenuOfLink.removeClass(CLASSES.HAS_SUBMENU_ACTIVE)
-        }
-      });
-
+      // Clicking on second level items 
+      if ( thisLevel === '1' ) {
+        checkIfItemsHaveClass(otherLinksAtThisLevel, CLASSES.IS_ACTIVE) ? parentMenuOfLink.addClass(CLASSES.HAS_SUBMENU_ACTIVE) : parentMenuOfLink.removeClass(CLASSES.HAS_SUBMENU_ACTIVE)
+      }
     });
+
   });
-};
+});
