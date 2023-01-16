48d52a0af091fb3df72a9a920de0f0fda5a3daa4
George Phillips <g.phillips@catchdigital.com>
2023-01-16 15:37:04 +0000

    CATCH-698: Tried to patch sass issue and added dart-sass.

--- scripts/css.js
+++ scripts/css.js
@@ -4,9 +4,12 @@ const exec = require('child_process').exec;
 const args = process.argv.slice(2);
 
 if (args.length < 4) {
-  console.error('\x1b[31m', 'Please provide paths to node_modules, the input and the output directory and output file');
+  console.error(
+    '\x1b[31m',
+    'Please provide paths to node_modules, the input and the output directory and output file'
+  );
 } else {
-  exec(`node ${args[0]}/.bin/node-sass ${args[1]} -o ${args[2]} --source-map true --include-path ${args[0]}`, (err) => {
+  exec(`npx sass -I ${args[0]} ${args[1]}:${args[2]}`, (err) => {
     if (err) {
       return console.error('\x1b[31m', err);
     }
