53e70302114de38505c233483554d4540e26b2ca
George Phillips <g.phillips@catchdigital.com>
2023-01-18 09:58:16 +0000

    CATCH-000: I am dumb.

--- scripts/css.js
+++ scripts/css.js
@@ -9,7 +9,7 @@ if (args.length < 4) {
     'Please provide paths to node_modules, the input and the output directory and output file'
   );
 } else {
-  exec(`npx sass -I ${args[0]} ${args[1]}:${args[2]}`, (err) => {
+  exec(`npx sass -I ${args[0]} ${args[1]}:${args[3]}`, (err) => {
     if (err) {
       return console.error('\x1b[31m', err);
     }
